98994399854beeb52e49df61a46381b41cf6c57a
Sebastian Zavala <sebastianzvla1@gmail.com>
2024-09-10 04:10:05 +0000

    s

--- models/incremental/inc_orders.sql
+++ models/incremental/inc_orders.sql
@@ -1,5 +1,12 @@
-select 
-    id as customer_id,
-    first_name,
-    last_name
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+
+    models:
+        +incremental_strat    
+}}
+
+select *
 from {{ source('jaffle_shop', 'customers') }}
